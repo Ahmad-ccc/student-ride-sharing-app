19c6808cdb1442d746d8d09d279bb3af185375fa
Ahmad-ccc <whitbeardznb@gmail.com>
2026-05-20 22:33:08 +0300

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,7 @@
-import type { Config } from 'jest';
-
-const config: Config = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
-};
-
-export default config;
+{
+  "preset": "ts-jest",
+  "testEnvironment": "node",
+  "testMatch": ["**/*.test.ts"],
+  "collectCoverage": true,
+  "coverageDirectory": "coverage"
+}
